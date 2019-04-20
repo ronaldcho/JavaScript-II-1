eea768cc0525ccfc5245cecc098ca3ae9022eb0a
ronaldcho <ronaldcho@gmail.com>
2019-04-19 22:14:54 -0700

    callback

--- assignments/callbacks.js
+++ assignments/callbacks.js
@@ -2,49 +2,79 @@
 
 const items = ['Pencil', 'Notebook', 'yo-yo', 'Gum'];
 
-/* 
 
   //Given this problem: 
-  
+/*  
   function firstItem(arr, cb) {
     // firstItem passes the first item of the given array to the callback function.
   }
-
   // Potential Solution:
-
   // Higher order function using "cb" as the call back
   function firstItem(arr, cb) {
+    console.log("This is array parameter contain [" + arr + "]" + cb);
     return cb(arr[0]);
   }
-
   // Function invocation 
   firstItem(items, function(first) {
     console.log(first)
   });
-
 */
-
 
 function getLength(arr, cb) {
   // getLength passes the length of the array into the callback.
+  return cb(arr.length);
 }
+getLength(items, function(first) {
+  console.log(first);
+})
 
 function last(arr, cb) {
   // last passes the last item of the array into the callback.
+  return cb(arr[arr.length - 1]);
 }
+last(items, function(first) {
+  console.log(first);
+})
 
 function sumNums(x, y, cb) {
   // sumNums adds two numbers (x, y) and passes the result to the callback.
+  return cb( x + y );
 }
+
+sumNums(2, 4, function(first){
+  console.log(first);
+})
+
 
 function multiplyNums(x, y, cb) {
   // multiplyNums multiplies two numbers and passes the result to the callback.
+  return cb( x * y );
 }
+multiplyNums(3, 9, function(first){
+  console.log(first);
+})
 
 function contains(item, list, cb) {
   // contains checks if an item is present inside of the given array/list.
   // Pass true to the callback if it is, otherwise pass false.
+     // console.log(list.indexOf(item));
+     // console.log(item);
+  
+  if (list.indexOf(item) == 1) {
+    // console.log(list.indexOf(item));
+   return cb(true); 
+  } else {
+      return cb(false);
+  }
 }
+
+contains('Bats', items, function(flag) {
+  console.log(flag); // should be false
+});
+
+contains('Notebook', items, function(flag) {
+  console.log(flag); // should be true
+});
 
 /* STRETCH PROBLEM */
 
@@ -52,4 +82,5 @@ function removeDuplicates(array, cb) {
   // removeDuplicates removes all duplicate values from the given array.
   // Pass the duplicate free array to the callback function.
   // Do not mutate the original array.
+  
 }
